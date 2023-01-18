94352b1cd985c31d692bad6f8d4a28e6fec723c1
Janer Vega <66845868+Ld-7@users.noreply.github.com>
2023-01-17 19:37:39 -0500

    config and autostart files

---
+++ autostart.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Background
+feh --bg-fill /usr/share/backgrounds/6.jpg
+
+# Compositor
+picom & disown
+
+# Wifi Icon
+nm-applet &
+
+# Volume icon
+
+# Start polkit agent from Gnome
